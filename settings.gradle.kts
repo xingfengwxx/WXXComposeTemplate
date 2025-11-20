c5d8367e684005d7f07211140021b7b7b411c78a
xingfengwxx <xingfengwxx@gmail.com>
2025-11-20 17:32:31 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,56 @@
+pluginManagement {
+    repositories {
+
+        // ✅ 阿里云镜像仓库（加速 Gradle 插件下载）
+        maven {
+            url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/public")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/google")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/jcenter")
+        }
+
+        maven {
+            url = uri("https://jitpack.io")
+        }
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        // ✅ 阿里云镜像仓库（用于依赖下载）
+        maven {
+            url = uri("https://maven.aliyun.com/repository/public")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/google")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/jcenter")
+        }
+        maven {
+            url = uri("https://jitpack.io")
+        }
+
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "WXXComposeTemplate"
+include(":app")
+ 
